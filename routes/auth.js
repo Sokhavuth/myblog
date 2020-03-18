8ecd4791690fe2ae6d4c6bd52795b28b28d83b72
Sokhavuth TIN <vuthdevelop@gmail.com>
2020-03-18 14:44:44 +0700

    Make it better

---
+++ routes/auth.js
@@ -0,0 +1,46 @@
+/* routes/auth.js */
+const MongoClient = require('mongodb').MongoClient;
+const url = require('./config').mongoDBurl;
+const ObjectId = require('mongodb').ObjectId
+
+exports.findById = function(id, cb) {
+  process.nextTick(function() {
+    MongoClient.connect(url, {useUnifiedTopology:true}, function(err, db){
+      if (err) throw err;
+      var dbo = db.db("mydb");
+      dbo.collection("users").find({_id:ObjectId(id)}).toArray(function(err, result) {
+        if (err) throw err;
+        if(result.length > 0){
+         var record = result[0];
+          cb(null, record);
+          db.close();
+        }else{
+          cb(new Error('User ' + id + ' does not exist'));
+          db.close();
+        }
+      });
+  
+    });
+
+  });
+}
+
+exports.findByUsername = function(username, cb) {
+  process.nextTick(function() {
+    MongoClient.connect(url, {useUnifiedTopology:true}, function(err, db){
+      if (err) throw err;
+      var dbo = db.db("mydb");
+      dbo.collection("users").find({username:username}).toArray(function(err, result) {
+        if (err) throw err;
+        if(result.length > 0){
+          var record = result[0];
+          db.close();
+          return cb(null, record);
+        }else{
+          db.close();
+          return cb(null, null);
+        }
+      });
+    });
+  });
+}
--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,13 @@
 /*routes/login.js*/
 const express = require('express');
 const router = express.Router();
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+var session = require('express-session');
+var ifLogedin = require('connect-ensure-login');
+
 const users = require('./users');
+const auth = require('./auth')
 
 router.use('/',function(req,res,next){
   users.getUsers().then(function(result){
@@ -12,8 +18,51 @@ router.use('/',function(req,res,next){
   });
 });
 
+passport.use(new Strategy(
+  function(username, password, cb) {
+    auth.findByUsername(username, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+}));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user._id);
+});
+  
+passport.deserializeUser(function(id, cb) {
+  auth.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+router.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+router.use(passport.initialize());
+router.use(passport.session());
+
 router.get('/', (req, res) => {
   res.render('login',{blogTitle:"Login"});
+});
+
+router.post('/',
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/login/dashboard');
+});
+
+router.get('/dashboard',
+  ifLogedin.ensureLoggedIn('/login'),
+  function(req, res){
+    res.render('dashboard', {blogTitle:"Dashboard"});
+});
+
+router.get('/logout',
+  function(req, res){
+    req.logout();
+    res.redirect('/');
 });
 
 module.exports = router;
