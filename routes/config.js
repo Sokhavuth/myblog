29245644b2e6e3d4fe9e9917dab0458363046f57
Sokhavuth TIN <vuthdevelop@gmail.com>
2020-04-13 13:56:20 +0700

    Make it better

--- routes/config.js
+++ routes/config.js
@@ -2,4 +2,4 @@
 exports.mongoDBurl = "mongodb+srv://sokhavuth:mypassword@khmerjs-jvhqu.mongodb.net/mydb?retryWrites=true&w=majority";
 //exports.mongoDBurl = "mongodb://localhost:27017/mydb";
 exports.postLimit = 15;
-exports.blogTitle = "World Express News";
+exports.blogTitle = "World Epress News";
