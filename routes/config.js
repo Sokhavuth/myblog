385a68aa8c51c6c7018f3bbcaeaa200d0f0aa22e
Sokhavuth <49101948+Sokhavuth@users.noreply.github.com>
2020-06-07 11:06:18 +0700

    Update config.js

--- routes/config.js
+++ routes/config.js
@@ -1,5 +1,5 @@
 /* routes/config.js */
-exports.mongoDBurl = "mongodb+srv://khmerjs:Password@khmerjs-jvhqu.mongodb.net/mydb?retryWrites=true&w=majority";
+exports.mongoDBurl = "mongodb+srv://{username}:{password}@khmerjs-jvhqu.mongodb.net/mydb?retryWrites=true&w=majority";
 //exports.mongoDBurl = "mongodb://localhost:27017/mydb";
 exports.postLimit = 15;
 exports.blogTitle = "Khmer Digital Era"; 
